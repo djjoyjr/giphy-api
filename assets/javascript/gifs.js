17bb0aa217d9d65b31edd71f8cab4fdd4d3a6feb
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2017-11-09 18:05:18 -0500

    ready for submission

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
   var tvShows = ["Arrested Development", "Breaking Bad", "Scrubs", "The Office", "Parks and Recreation", "Mr Robot", "Always Sunny in Philadephia", "The Simpsons", "The Good Place", "Modern Family"];
-
+    //this function makes the buttons appear at the top of the DOM
     function makeButtons (){
       $("#show-buttons").empty();
       for (var i = 0; i < tvShows.length; i++) {
         var newButton = $("<button>");
-        newButton.addClass("show");
+        newButton.addClass("show, btn-change6");
         newButton.attr("data-name", tvShows[i]);
         newButton.text(tvShows[i]);
         $("#show-buttons").append(newButton);
       };
     };
-
+    //this listens for the submit button to be clicked when user enters a new show name, then makes a new button
   $("#submit-show").on("click", function(event) {
     event.preventDefault();
     var newShow = $("#show-input").val().trim();
     tvShows.push(newShow);
     makeButtons();
   });
-
+  //changes from still pic to animated gif when an image is clicked
   function animateGif() {
     if ($(this).attr("data-state") ==="still") {
       $(this).attr("src", $(this).attr('data-animate'));
@@ -29,7 +29,7 @@ $(document).ready(function(){
       $(this).attr("data-state", "still");
     }
   }
-
+  //this function populates the screen with gifs when a button is pressed
   function changeTheme() {
       var show = $(this).attr("data-name");
       var changeURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=K4KU6OMUATEW1UmbAjY53j4ZGoSI0e9Y";
@@ -47,16 +47,13 @@ $(document).ready(function(){
             var rating = response.data[i].rating;
             var displayRating = $("<h2>").text("Rating: " + rating);
             showDiv.append(displayRating);
-            image = $("<img>").attr({"src": image, "id": id, "height":250, "data-still": image, "data-animate": animated, "data-state": 'still', "class": 'gif'});
+            image = $("<img>").attr({"src": image, "id": id, "height":200, "data-still": image, "data-animate": animated, "data-state": 'still', "class": 'gif'});
             showDiv.append(image);
             $("#show-gifs").append(showDiv);
           }
       });
   }
-
     $(document).on("click", "img", animateGif);
     $(document).on("click", "button", changeTheme);
-
 makeButtons();
-});
-//end of document ready function
+}); //end of document ready function
